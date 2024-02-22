5a1c8da5d858ed500ec3c457fb54d797d9b9e8b4
joordonezo <59375583+joordonezo@users.noreply.github.com>
2024-02-22 01:04:38 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-5cvw1j
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/joordonezo/stackblitz-starters-5cvw1j)
---
+++ src/app/app.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgxComboboxModule } from 'ngx-combobox';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, NgxComboboxModule],
+})
+export class AppModule {}
